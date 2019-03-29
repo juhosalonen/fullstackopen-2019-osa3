ae21aa8243b75157fd1c8222cb2908c7e89edd46
Salonen Juho M <juho.m.salonen@elisa.fi>
2019-03-29 11:56:00 +0200

    add production build

---
+++ build/precache-manifest.bb92db1ed57e664fec69748a340028c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ae1bdc05746e682100e",
+    "url": "/static/js/main.0c3e1fa9.chunk.js"
+  },
+  {
+    "revision": "8839adfefd4f764430da",
+    "url": "/static/js/2.d5acc9c7.chunk.js"
+  },
+  {
+    "revision": "6ae1bdc05746e682100e",
+    "url": "/static/css/main.3854c229.chunk.css"
+  },
+  {
+    "revision": "f844bbb589091818cc7125b13f9938bc",
+    "url": "/index.html"
+  }
+];
